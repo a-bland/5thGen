e6a538ccfadc0c33612a6f5627a99aa7fc2bc98c
Annabel Bland <thisisannabelbland@gmail.com>
2021-02-21 22:34:03 -0500

    readding user auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Typography } from 'antd';
+
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import aws_exports from './aws-exports.js'; // specify the location of aws-exports.js file on your project
+Amplify.configure(aws_exports);
+
+const { Title } = Typography;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>Gen Five</Title>
     </div>
+
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
