9033b872c5df72b1f8a6111eddb6c4eb12f89071
Liam May <liam_may@knights.ucf.edu>
2021-03-01 15:18:25 -0500

    Adds header and paragraph to index.html, App.css. Also adds testing code to App.test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders the correct content", () => 
+{
+  // Render a React component to the DOM
+  const root = document.createElement("div");
+  ReactDOM.render(<App />, root);
+
+  // Use DOM APIs (queryselector) to make assertions
+  expect(root.querySelector("h1"), textContent).toBe("Header");
 });
