0d9da82366ee8c247ca1c400e12f9f691d438ac0
Annabel Bland <thisisannabelbland@gmail.com>
2021-03-03 15:10:37 -0500

    added custom selects for class and race

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -3,7 +3,7 @@ import 'antd/dist/antd.css';
 import { Typography } from 'antd';
 
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
-import aws_exports from '../aws-exports.js'; // specify the location of aws-exports.js file on your project
+import aws_exports from '../aws-exports.js';
 Amplify.configure(aws_exports);
 
 const { Title } = Typography;
@@ -13,11 +13,47 @@ function Creation() {
         <div className="Creation">
             <Title>Create Your Character</Title>
             <p>Choose wisely!</p>
-            <p>Character name: </p>
+            <form action="Creation" method="post" id="CharacterCreation">
+                <input type="text" placeholder="Character Name"></input>
+                <br /><br />
+                <div class="custom-select">
+                    <select>
+                        <option value="0">Select race:</option>
+                        <option value="1">Dwarf</option>
+                        <option value="2">Elf</option>
+                        <option value="3">Halfing</option>
+                        <option value="4">Human</option>
+                        <option value="5">Dragonborn</option>
+                        <option value="6">Gnome</option>
+                        <option value="7">Half-Elf</option>
+                        <option value="8">Half-Orc</option>
+                        <option value="9">Tiefling</option>
+                    </select>
+                </div>
+                <br /><br />
+                <div class="custom-select">
+                    <select>
+                        <option value="0">Select class:</option>
+                        <option value="1">Barbarian</option>
+                        <option value="2">Bard</option>
+                        <option value="3">Cleric</option>
+                        <option value="4">Druid</option>
+                        <option value="5">Fighter</option>
+                        <option value="6">Monk</option>
+                        <option value="7">Paladin</option>
+                        <option value="8">Ranger</option>
+                        <option value="9">Rogue</option>
+                        <option value="10">Sorcerer</option>
+                        <option value="11">Warlock</option>
+                        <option value="12">Wizard</option>
+                    </select>
+                </div>
+            </form>
+            <br /><br />
             <a href="/">
                 <button> Save Character </button>
             </a>
-        </div>
+        </div >
 
     );
 }
