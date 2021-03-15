254686e713fe88504c41f805d77985fc05b189a1
Annabel Bland <thisisannabelbland@gmail.com>
2021-03-15 16:39:54 -0400

    can now roll any number of any kind of dice

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -15,7 +15,16 @@ function Battle() {
                 <input type="radio" id="disadvantage" name="rollType"></input>
                 <label for="disadvantage"> Disadvantage </label>
             </form>
-            <button onClick={rollDice}> Roll D20 </button>
+            <button onClick={rolld20}> Roll D20 </button>
+            <br /><br />
+            <label for="rollNumber">Number of Dice</label>
+            <input type="number" id="rollNumber" defaultValue="1" min="1"></input>
+            <br />
+            <label for="diceType">Type of Dice</label>
+            <input type="number" id="diceType" defaultValue="6" min="2" step="2" max="100"></input>
+            <br />
+            <button onClick={roll}>Roll</button>
+
             <br /><br />
             <a href="/">
                 <button> End Battle </button>
@@ -24,7 +33,7 @@ function Battle() {
 
     );
 }
-function rollDice() {
+function rolld20() {
     var resultOne = (Math.random() * 20) + 1;
     resultOne = resultOne - (resultOne % 1);
     if (document.getElementById('standard').checked) {
@@ -51,5 +60,16 @@ function rollDice() {
         }
     }
 
+}
+function roll() {
+    var numDice = document.getElementById("rollNumber").value;
+    var diceType = document.getElementById("diceType").value;
+    var sum = 0;
+    for (var i = 0; i < numDice; i++) {
+        var die = Math.random() * diceType + 1;
+        die = die - (die % 1);
+        sum += die;
+    }
+    alert(sum);
 }
 export default Battle;
