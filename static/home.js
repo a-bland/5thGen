b623eec4a5017d26eae4c0ad9cc336ea13ab6c5d
Annabel Bland <thisisannabelbland@gmail.com>
2021-03-27 18:09:02 -0400

    can toggle party character visibility

--- static/home.js
+++
@@ -1,9 +0,0 @@
-function showPartyForm() {
-    document.getElementById("partyForm").style.display = "block";
-    //document.getElementById("cancelParty").style.display = "inline";
-}
-function hidePartyForm() {
-    window.location.href = 'http://127.0.0.1:8000/home'
-    document.getElementById("partyForm").style.display = "none";
-    //document.getElementById("cancelParty").style.display = "none";
-}
